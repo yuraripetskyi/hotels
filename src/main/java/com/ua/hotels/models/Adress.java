8a1182324494498ac423c1b83c5fb8a0f76ecce9
yuraripetskyi <31436120+yuraripetskyi@users.noreply.github.com>
2018-08-22 05:23:15 -0700

    redirect registr

--- src/main/java/com/ua/hotels/models/Adress.java
+++ src/main/java/com/ua/hotels/models/Adress.java
@@ -1,4 +1,62 @@
 package com.ua.hotels.models;
 
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "hotel")
+@Entity
 public class Adress {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  int id;
+    private String street;
+    private String city;
+    private String house;
+
+    @OneToOne(
+            cascade = CascadeType.REFRESH,
+            fetch = FetchType.LAZY
+    )
+    private Hotel hotel;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
 }
