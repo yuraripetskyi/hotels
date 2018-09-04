c9c92c86925eff9de506ccf38d51fe80daf28463
LevNakon <lionsoul887@gmail.com>
2018-09-04 13:03:42 +0300

    MainController update_3

--- src/main/java/com/ua/hotels/controllers/SearchController.java
+++ src/main/java/com/ua/hotels/controllers/SearchController.java
@@ -16,6 +16,8 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 @Controller
 public class SearchController {
 
@@ -32,14 +34,14 @@ public class SearchController {
         model.addAttribute("user",customer);
         if (param.length == 1){
             String parametr = param[0];
-            Set<Customer> byOneParam = findByOneParam(parametr);
+            Set<Customer> byOneParam = findByOneParamByStream(parametr);
             model.addAttribute("users", byOneParam);
             return "admin" ;
         }
         else if (param.length == 2){
             String p1 = param[0];
             String p2 = param[1];
-            Set<Customer> byTwoParam = findByTwoParam(p1, p2);
+            Set<Customer> byTwoParam = findByTwoParamByStream(p1, p2);
             model.addAttribute("users", byTwoParam);
             return "admin";
         }else if(param.length >= 3){
@@ -50,39 +52,70 @@ public class SearchController {
     }
 
 
-    public Set<Customer> findByOneParam(String parametr){
-        List<Customer> allByName = customerDAO.findAllByName(parametr);
-        List<Customer> allBySurname = customerDAO.findAllBySurname(parametr);
-        allByName.addAll(allBySurname);
-        Set<Customer> allByName1 = new HashSet<>(allByName);
+//    public Set<Customer> findByOneParam(String parametr){
+//        List<Customer> allByName = customerDAO.findAllByName(parametr);
+//        List<Customer> allBySurname = customerDAO.findAllBySurname(parametr);
+//        allByName.addAll(allBySurname);
+//        Set<Customer> allByName1 = new HashSet<>(allByName);
+//        Customer user = MainController.findActiveUser();
+//        int id = user.getId();
+//        Iterator<Customer> iter = allByName1.iterator();
+//        while (iter.hasNext()){
+//            Customer next = iter.next();
+//            if(next.getId()== id){
+//                iter.remove();
+//            }
+//        }
+//        return allByName1;
+//    }
+
+    public Set<Customer> findByOneParamByStream(String parametr){
+        Set<Customer> byOneParam = customerDAO.findAll().stream().filter(customer -> customer.getName().equals(parametr) || customer.getSurname().equals(parametr)).collect(Collectors.toSet());
         Customer user = MainController.findActiveUser();
         int id = user.getId();
-        Iterator<Customer> iter = allByName1.iterator();
+        Iterator<Customer> iter = byOneParam.iterator();
         while (iter.hasNext()){
             Customer next = iter.next();
             if(next.getId()== id){
                 iter.remove();
             }
         }
-        return allByName1;
+        return byOneParam;
     }
 
-    public Set<Customer> findByTwoParam(String p1 , String p2){
-        List<Customer> list1 = customerDAO.findAllByNameAndSurname(p1, p2);
-        List<Customer> list2 = customerDAO.findAllByNameAndSurname(p2, p1);
-        list1.addAll(list2);
-        Set<Customer> list = new HashSet<>(list1);
+    public Set<Customer> findByTwoParamByStream(String p1 , String p2){
+        Set<Customer> byTwoParam = customerDAO.findAll().stream()
+                .filter(customer -> (customer.getName().equals(p1) && customer.getSurname().equals(p2)) ||
+                        (customer.getName().equals(p2) && customer.getSurname().equals(p1))).collect(Collectors.toSet());
         Customer user = MainController.findActiveUser();
         int id = user.getId();
-        Iterator<Customer> iter = list.iterator();
+        Iterator<Customer> iter = byTwoParam.iterator();
         while (iter.hasNext()){
             Customer next = iter.next();
             if(next.getId()== id){
                 iter.remove();
             }
         }
-        return list;
+        return byTwoParam;
+
     }
+
+//    public Set<Customer> findByTwoParam(String p1 , String p2){
+//        List<Customer> list1 = customerDAO.findAllByNameAndSurname(p1, p2);
+//        List<Customer> list2 = customerDAO.findAllByNameAndSurname(p2, p1);
+//        list1.addAll(list2);
+//        Set<Customer> list = new HashSet<>(list1);
+//        Customer user = MainController.findActiveUser();
+//        int id = user.getId();
+//        Iterator<Customer> iter = list.iterator();
+//        while (iter.hasNext()){
+//            Customer next = iter.next();
+//            if(next.getId()== id){
+//                iter.remove();
+//            }
+//        }
+//        return list;
+//    }
 
     @GetMapping("/guest/{username}")
     public String guestPage(@PathVariable String username, Model model) {
