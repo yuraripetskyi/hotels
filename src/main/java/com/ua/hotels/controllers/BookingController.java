36313f909ec32e73080f759a9009dc0d709eb198
andrikash <dizhakandrik@gmail.com>
2018-09-21 14:52:36 +0300

    Optimized filter code.

--- src/main/java/com/ua/hotels/controllers/BookingController.java
+++ src/main/java/com/ua/hotels/controllers/BookingController.java
@@ -6,18 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.jws.WebParam;
-import javax.validation.constraints.Null;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 @Controller
 public class BookingController {
@@ -27,27 +21,19 @@ public class BookingController {
 
     @GetMapping("/main")
     private String MainPage(@RequestParam(required = false, defaultValue = "") String finder,
-                            @RequestParam(required = false, defaultValue = "") String countOfGuests,
+                            @RequestParam(required = false,defaultValue = "") Integer countOfGuests,
                             @RequestParam(required = false, defaultValue = "") String filter,
                             Model model) {
         List<Room> rooms = new ArrayList<>();
-        List<Room> all = roomDAO.findAll();
-        // Пошук румів по кількості кімнат
-            List<Room> rooms1 = all.stream()
-                    .filter(room -> Integer.toString(room.getRoominess()).equals(countOfGuests)).collect(Collectors.toList());
-            //Пошук румів по місту чи назві готелю
-        List<Room> сityRoom = rooms1.stream()
-                //Тут буде помилка якщо назва готелю буде така ж як назва міста, тоді готелі виведе двічі
-                .filter(room -> room.getHotel().getCity().equals(finder)).collect(Collectors.toList());
-        rooms.addAll(rooms1.stream()
-                   .filter(room -> room.getHotel().getName().equals(finder)).collect(Collectors.toList()));
-            //Фільтрування за цінами
-        if(filter.equals("Descending")){
-            rooms.addAll(сityRoom.stream().sorted((o1, o2) -> o2.getPrice()-o1.getPrice()).collect(Collectors.toList()));
-        }if(filter.equals("Ascending")){
-            rooms.addAll(сityRoom.stream().sorted((o1, o2) -> o1.getPrice()-o2.getPrice()).collect(Collectors.toList()));
-        }
 
+        //Пошук румів по місту чи назві готелю і кількості кімнат відразу, я подумав що краще зробити це все на ДАО рівні і дуже круто тепер виглядає
+        List<Room> roomList = roomDAO.findAllByRoominessAndHotelCityOrRoominessAndHotelName(countOfGuests,finder,countOfGuests,finder);
+        //Фільтрування за цінами
+        if(filter.equals("Descending")){
+            rooms.addAll(roomList.stream().sorted((o1, o2) -> o2.getPrice()-o1.getPrice()).collect(Collectors.toList()));
+        }if(filter.equals("Ascending")){
+            rooms.addAll(roomList.stream().sorted((o1, o2) -> o1.getPrice()-o2.getPrice()).collect(Collectors.toList()));
+        }
         model.addAttribute("rooms", rooms);
         return "main";
     }
--- src/main/java/com/ua/hotels/dao/RoomDAO.java
+++ src/main/java/com/ua/hotels/dao/RoomDAO.java
@@ -3,5 +3,10 @@ package com.ua.hotels.dao;
 import com.ua.hotels.models.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Struct;
+import java.util.List;
+
 public interface RoomDAO extends JpaRepository<Room,Integer> {
+    //Потрібно шукати за кімнатими, а потім за містом або назвою, бо кімнати відфільтровують зайві запити
+    List<Room> findAllByRoominessAndHotelCityOrRoominessAndHotelName(Integer roominess,String finder,Integer room, String name);
 }
