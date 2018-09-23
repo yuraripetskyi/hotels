7567668430b611f48c53a67d151b13e1cd0a5aa6
yuraripetskyi <yuraripeckiy@gmail.com>
2018-09-23 09:13:07 +0300

    boooking boy

--- src/main/java/com/ua/hotels/controllers/BookingController.java
+++ src/main/java/com/ua/hotels/controllers/BookingController.java
@@ -1,5 +1,6 @@
 package com.ua.hotels.controllers;
 
+import com.ua.hotels.dao.BookDAO;
 import com.ua.hotels.dao.RoomDAO;
 import com.ua.hotels.models.Book;
 import com.ua.hotels.models.Customer;
@@ -12,7 +13,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.GeneratedValue;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -22,16 +27,26 @@ public class BookingController {
 
     @Autowired
     private RoomDAO roomDAO;
+    @Autowired
+    private BookDAO bookDAO;
 
     @GetMapping("/main")
+    private String Mainpage(){
+        return "main";
+    }
+    @PostMapping("/main")
     private String MainPage(@RequestParam(required = false, defaultValue = "") String finder,
                             @RequestParam(required = false,defaultValue = "") Integer countOfGuests,
                             @RequestParam(required = false, defaultValue = "") String filter,
-                            Model model) {
+                            @RequestParam(required = false, defaultValue = "") String from_date,
+                            @RequestParam(required = false, defaultValue = "") String to_date,
+                            Model model) throws ParseException {
         List<Room> rooms = new ArrayList<>();
         List<Room> roomList = roomDAO.findAllByRoominessAndHotelCityOrRoominessAndHotelName(countOfGuests,finder,countOfGuests,finder);
         rooms.addAll(filterByPrice(filter, roomList));
-        model.addAttribute("rooms", rooms);
+        List<Room> free_rooms = commpareDates(rooms, from_date, to_date);
+
+        model.addAttribute("rooms", free_rooms);
         return "main";
     }
 
@@ -43,6 +58,8 @@ public class BookingController {
         model.addAttribute("hotel", room.getHotel());
         return "book";
     }
+
+
 
     @PostMapping("/book/room/{id}")
     public String book(@PathVariable int id,
@@ -58,6 +75,44 @@ public class BookingController {
         return "user";
     }
 
+
+    private List<Room> commpareDates(List<Room> rooms, String from_date, String to_date) throws ParseException {
+
+            Date from = new SimpleDateFormat("MM/dd/yyyy").parse(from_date);
+
+
+
+            Date to = new SimpleDateFormat("MM/dd/yyyy").parse(to_date);
+
+        List<Book> book = bookDAO.findAll();
+
+        for (Room room: rooms) {
+            List<Book> books = room.getBook();
+            for (Book boo :books) {
+
+                  Date book_from =  new SimpleDateFormat("MM/dd/yyyy").parse(boo.getDate_from());
+                    Date book_to = new SimpleDateFormat("MM/dd/yyyy").parse(boo.getDate_to());
+
+                if(from.compareTo(book_from) > 0 && from.compareTo(book_to) < 0){
+                    Iterator itr = rooms.iterator();
+                    while (itr.hasNext())
+                    {
+                            itr.remove();
+                    }
+                }
+                if(to.compareTo(book_from) > 0 && to.compareTo(book_to) < 0){
+                    Iterator itr = rooms.iterator();
+                    while (itr.hasNext())
+                    {
+                        itr.remove();
+                    }
+                }
+
+            }
+        }
+
+        return rooms;
+    }
 
     private List<Room> filterByPrice(String filter, List<Room> roomList){
         List<Room> rooms = new ArrayList<>();
