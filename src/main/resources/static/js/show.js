3284c9c49f97bdb1dbc05e699185bcd7936a4629
andrikash <dizhakandrik@gmail.com>
2018-10-02 14:36:51 +0300

    g

--- src/main/resources/static/js/show.js
+++ src/main/resources/static/js/show.js
@@ -1,13 +1,13 @@
-let submit = document.getElementById("showForgotBox");
-    let box = document.getElementById("forgotBox");
-    let i = 0;
-    submit.onclick = function(){
-        $('showForgotBox').toggle(function(){
-            $('showForgotBox').text('Hide');
-        },function(){
-            $('showForgotBox').text('Troubles with enter?');
-        });
-    };
+// let submit = document.getElementById("showForgotBox");
+//     let box = document.getElementById("forgotBox");
+//     let i = 0;
+//     submit.onclick = function(){
+//         $('showForgotBox').toggle(function(){
+//             $('showForgotBox').text('Hide');
+//         },function(){
+//             $('showForgotBox').text('Troubles with enter?');
+//         });
+//     };
 
 
 
