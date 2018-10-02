195dd5c10a54c0dfc8a6559d251588ee1b481e05
andrikash <dizhakandrik@gmail.com>
2018-10-02 14:17:18 +0300

    Change some

--- src/main/resources/static/js/show.js
+++ src/main/resources/static/js/show.js
@@ -1,23 +1,16 @@
 let submit = document.getElementById("showForgotBox");
-let box = document.getElementById("forgotBox");
-let i = 0;
-submit.onclick = function(){
-
-    // i++;
-    // if(i%2 == 1){
-        submit.text("hide");
-        box.style.display="block";
-    // }else {
-    //     submit.text("Troubles with enter");
-    //     box.style.display="none";
-    // }
-
-};
+    let box = document.getElementById("forgotBox");
+    let i = 0;
+    submit.onclick = function(){
+        $('showForgotBox').toggle(function(){
+            $('showForgotBox').text('Hide');
+        },function(){
+            $('showForgotBox').text('Troubles with enter?');
+        });
+    };
 
 
 
-// $('showForgotBox').toggle(function(){
-//     $('showForgotBox').text('Hide');
-// },function(){
-//     $('showForgotBox').text('Troubles with enter?');
-// });
+
+
+
