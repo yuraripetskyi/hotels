6a9853a2149bdf89b8eba8fabb9cf60ec09dd28a
andrikash <dizhakandrik@gmail.com>
2018-10-11 10:28:20 +0300

    Find by dates well done.

--- src/main/java/com/ua/hotels/controllers/LoginForgetController.java
+++ src/main/java/com/ua/hotels/controllers/LoginForgetController.java
@@ -41,7 +41,7 @@ public class LoginForgetController {
     private CustomerValidator customerValidator;
 
 
-    public void sendMail(String email, String subject, String text) throws javax.mail.MessagingException {
+    public  void sendMail(String email, String subject, String text) throws javax.mail.MessagingException {
         MimeMessage mimeMessage = sender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
         helper.setText(text, true);
--- src/main/java/com/ua/hotels/dao/RoomDAO.java
+++ src/main/java/com/ua/hotels/dao/RoomDAO.java
@@ -4,9 +4,10 @@ import com.ua.hotels.models.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.sql.Struct;
+import java.util.LinkedList;
 import java.util.List;
 
 public interface RoomDAO extends JpaRepository<Room,Integer> {
     //Потрібно шукати за кімнатими, а потім за містом або назвою, бо кімнати відфільтровують зайві запити
-    List<Room> findAllByRoominessAndHotelCityOrRoominessAndHotelName(Integer roominess,String finder,Integer room, String name);
+    LinkedList<Room> findAllByRoominessAndHotelCityOrRoominessAndHotelName(Integer roominess, String finder, Integer room, String name);
 }
